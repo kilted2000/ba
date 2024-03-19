b982a700f58cd767fe6cce8da9e92a856910d62d
Richard <richardlwhittington@gmail.com>
2024-03-19 09:13:26 -0500

    add all

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const port = process.env.PORT || 8080;
+
+const jwtCheck = auth({
+    audience: 'https://www.barkamigos.com/',
+    issuerBaseURL: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+});
+
+// enforce on all endpoints
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
+
+app.listen(port);
+
+console.log('Server currently running on port ', port);
