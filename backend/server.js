670f8536018be96a325a446a089d6b28f86ed1b7
Richard <richardlwhittington@gmail.com>
2024-04-08 22:15:14 -0500

    add prisma

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,29 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const { auth } = require('express-oauth2-jwt-bearer');
+//import { auth } from'express-oauth2-jwt-bearer';
 
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 8080;
 
-const jwtCheck = auth({
-    audience: 'https://www.barkamigos.com/',
-    issuerBaseURL: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/',
-    tokenSigningAlg: 'RS256'
-});
+// const jwtCheck = auth({
+//     audience: 'https://www.barkamigos.com/',
+//     issuerBaseURL: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/',
+//     tokenSigningAlg: 'RS256'
+// });
 
-// enforce on all endpoints
-app.use(jwtCheck);
+// // enforce on all endpoints
+// app.use(jwtCheck);
 
-app.get('/authorized', function (req, res) {
-    res.send('Secured Resource');
-});
+// app.get('/authorized', function (req, res) {
+//     res.send('Secured Resource');
+// });
+
+app.get("/", async (req, res) => {
+    res.json({ message: "success" });
+  });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
