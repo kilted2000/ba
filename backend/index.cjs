27708170e0320eadf05087ea131b8bddec309595
Richard <richardlwhittington@gmail.com>
2024-05-18 22:10:08 -0500

    add cs

--- backend/index.cjs
+++ backend/index.cjs
@@ -71,6 +71,15 @@ app.get('/authorized', function (req, res) {
     res.send('Secured Resource');
 });
 
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+
 app.get("/", async (req, res) => {
     res.json({ message: "Great Job! It works!" });
   });
