1274edb64418b0be925442fa848311400ab00fab
Richard <richardlwhittington@gmail.com>
2024-05-31 23:26:16 -0500

    console log error

--- backend/index.cjs
+++ backend/index.cjs
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { PrismaClient } = require('@prisma/client');
 const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
-
+const port = process.env.PORT || 8080;
 const app = express();
 const prisma = new PrismaClient();
 const userRoutes = require('./src/routes/userRoutes.cjs');
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(express.json());
 
-const port = process.env.PORT || 8080;
+
 
 const jwtCheck = auth({
   audience: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/api/v2/',
@@ -79,57 +79,4 @@ module.exports = app;
 // // });
 
 // // async function main() {
-// //   const allUsers = await prisma.userprofile.findMany();
-// //   app.get('/users', (req, res) => {
-// //     res.json(allUsers);
-// //   });
-// // }
-// // main()
-// //   .then(() => {
-// //     console.log('Starting server...');
-// //     app.listen(port, () => {
-// //       console.log(`Server running on port ${port}`);
-// //     });
-// //   })
-
-// // main()
-// //   .then(() => {
-// //     app.listen(port, () => {
-// //       console.log(`Server running on port ${port}`);
-// //     });
-// //   })
-//   // .catch(async (e) => {
-//   //   console.error(e);
-//   //   await prisma.$disconnect();
-//   //   process.exit(1);
-//   // });
-
-
-
-
-// const jwtCheck = auth({
-//     audience: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/api/v2/',
-//     issuerBaseURL: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/',
-//     algorithms: ['RS256'],
-//     jwksUri: 'https://dev-txcw3jo08qihcb5z.us.auth0.com/.well-known/jwks.json'
-// });
-
-// app.use(jwtCheck);
-// app.use('/', userRoutes);
-// app.get('/authorized', function (req, res) {
-//     res.send('Secured Resource');
-// });
-
-
-
-
-// app.get("/", async (req, res) => {
-//     res.json({ message: "Great Job! It works!" });
-//   });
-//   // app.get('/users', async (req, res) => {
-//   //   const allUsers = await prisma.userprofile.findMany();
-//   //   res.json(allUsers);
-//   // });
-// app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// module.exports = app;
+// //   const allUsers = await prisma.userprofile.findMan
